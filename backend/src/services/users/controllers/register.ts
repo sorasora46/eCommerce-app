6d4b47e9851b8fbeb6babebf066f0ac1000fe3f3
sora-k46 <sorrawit456789@gmail.com>
2022-12-27 03:21:37 +0700

    Create authUser when register with hashed password

--- backend/src/services/users/controllers/register.ts
+++ backend/src/services/users/controllers/register.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import { User } from "../user.model.js";
 import { nanoid } from "nanoid";
-import mongoose from "mongoose";
+import mongoose, { StringSchemaDefinition } from "mongoose";
+import { authUser } from "../../auth/authUser.model.js";
+import * as bcrypt from "bcrypt";
 
 export const createUser = async (
   user_email: string,
   user_fname: string,
   user_lname: string,
   user_dateOfBirth: Date,
+  user_password: string,
   user_profileImage: string
 ) => {
   await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
@@ -37,18 +40,34 @@ export const createUser = async (
     .then()
     .catch((err) => console.log(err));
 
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(user_password, salt);
+
+  const newAuthUser = await authUser.create({
+    email: user_email,
+    hashedPassword: hashedPassword,
+    salt: salt,
+  });
+
+  newAuthUser
+    .save()
+    .then()
+    .catch((err) => console.log(err));
+
   return newUser;
 };
 
 export const register = async (req: Request, res: Response) => {
   try {
-    const { email, fname, lname, dateOfBirth, profileImage } = req.body;
-    if (email && fname && lname && dateOfBirth) {
+    const { email, fname, lname, dateOfBirth, profileImage, password } =
+      req.body;
+    if (email && fname && lname && dateOfBirth && password) {
       const result = await createUser(
         email,
         fname,
         lname,
         dateOfBirth,
+        password,
         profileImage
       );
       return res.json(result);
