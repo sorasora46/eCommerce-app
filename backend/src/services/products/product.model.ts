6e8e3e81cf4e52893d2bc9569c914fe0edf64169
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 22:40:58 +0700

    Change pImage to be array

--- backend/src/services/products/product.model.ts
+++ backend/src/services/products/product.model.ts
@@ -4,7 +4,7 @@ interface Product {
   productId: string;
   pName: string;
   pPrice: number;
-  pImage: string;
+  pImages: string[];
   pOwnerId: string;
 }
 
@@ -12,7 +12,7 @@ const productSchema = new Schema<Product>({
   productId: { type: String, required: true },
   pName: { type: String, required: true },
   pPrice: { type: Number, required: true },
-  pImage: { type: String, required: true },
+  pImage: { type: Array<String>, required: true },
   pOwnerId: { type: String, required: true },
 });
 
