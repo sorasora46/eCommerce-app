56aaeeb52081aeeb477f825a25b2281e98ca2746
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 14:38:39 +0700

    Add email and username to schema

--- backend/src/users/user.schema.ts
+++ backend/src/users/user.schema.ts
@@ -6,6 +6,12 @@ export type UserDocument = HydratedDocument<User>;
 @Schema()
 export class User {
   @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
   fname: string;
 
   @Prop({ required: true })
