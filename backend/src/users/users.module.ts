f23180467c544b5ed72f4a9a4b52e7e4a57d03db
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 14:16:02 +0700

    Import users.service

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User, UserSchema } from './user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './users.controller';
 
 @Module({
-  providers: [UsersService]
+  imports: [MongooseModule.forFeature([{name: "User", schema: UserSchema}])],
+  providers: [UsersService],
+  controllers: [UsersController]
 })
 export class UsersModule {}
