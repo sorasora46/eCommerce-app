67a0e5643ed554b1069a57e2ef26e59cb99cf31f
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 19:12:15 +0700

    Import router from services

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import express, { Express } from "express";
-import authenticationRoute from "./routers/auth/index"
+import authenticationRoute from "./services/auth/index";
+import cartRoute from "./services/carts/index";
+import productRoute from "./services/products/index";
+import shopRoute from "./services/shops/index";
+import transactionRoute from "./services/transactions/index";
+import userRoute from "./services/users/"
 
 const app: Express = express();
 const PORT = 8000;
 
-app.use("/login", authenticationRoute)
+app.use("/login", authenticationRoute);
+app.use("/cart", cartRoute);
+app.use("/product", productRoute);
+app.use("/shop", shopRoute);
+app.use("/transaction", transactionRoute);
+app.use("/user", userRoute);
 
-app.listen(PORT, console.log(`Server running at PORT:${PORT}`))
+app.listen(PORT, console.log(`Server running at PORT:${PORT}`));
