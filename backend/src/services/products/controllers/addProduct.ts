83c51e35ab416c24216a0636c2eea2c817d89110
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 22:39:59 +0700

    Create controller for addProduct

---
+++ backend/src/services/products/controllers/addProduct.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { Product } from "../product.model.js";
+import { nanoid } from "nanoid";
+import mongoose from "mongoose";
+
+export const createProduct = async (
+  pName: string,
+  pPrice: number,
+  pImages: string[],
+  pOwner: string
+) => {
+  await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
+
+  const sameOwnerAndName = await Product.findOne({
+    pOwnerId: pOwner,
+    pName: pName,
+  });
+  if (sameOwnerAndName) throw new Error("Product already exist [Name, Owner]");
+
+  if (pImages.length === 0)
+    throw new Error("Product must include atleast 1 picture");
+
+  const newProduct = await Product.create({
+    productId: nanoid(),
+    pImage: pImages,
+    pName: pName,
+    pOwnerId: pOwner,
+    pPrice: pPrice,
+  });
+
+  newProduct
+    .save()
+    .then()
+    .catch((err) => console.log(err));
+
+  return newProduct;
+};
+
+export const addProduct = async (req: Request, res: Response) => {
+  try {
+    const { pOwner, pPrice, pName, pImages } = req.body;
+    const result = await createProduct(pName, pPrice, pImages, pOwner);
+    return res.json(result);
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).json({ message: error.toString() });
+  }
+};
