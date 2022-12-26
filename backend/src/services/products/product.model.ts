5186ff10e3f73edae592c9139689d2a725fefba8
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 22:49:34 +0700

    Change pImage type

--- backend/src/services/products/product.model.ts
+++ backend/src/services/products/product.model.ts
@@ -12,7 +12,7 @@ const productSchema = new Schema<Product>({
   productId: { type: String, required: true },
   pName: { type: String, required: true },
   pPrice: { type: Number, required: true },
-  pImage: { type: Array<String>, required: true },
+  pImages: { type: [String], required: true },
   pOwnerId: { type: String, required: true },
 });
 
