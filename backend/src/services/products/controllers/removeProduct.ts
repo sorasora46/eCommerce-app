8c81c4c549553a18bb160526218017c806ad431c
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 23:10:58 +0700

    Create controller for removeProduct

---
+++ backend/src/services/products/controllers/removeProduct.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { Product } from "../product.model.js";
+import mongoose from "mongoose";
+
+export const deleteProduct = async (shopId: string, productId: string) => {
+  await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
+
+  const foundProduct = await Product.findOne({
+    pOwnerId: shopId,
+    productId: productId,
+  });
+  if (!foundProduct) throw new Error("Product not found");
+
+  const result = await Product.remove({ productId: productId });
+  return result;
+};
+
+export const removeProduct = async (req: Request, res: Response) => {
+  try {
+    const { shopId, productId } = req.body;
+    const result = await deleteProduct(shopId, productId);
+    return res.json(result);
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).json({ message: error.toString() });
+  }
+};
