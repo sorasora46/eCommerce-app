fef61b0fe1f167dc9191a903472a3eafc05785a3
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 14:15:45 +0700

    Implement users.service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,17 @@
+import { Model } from "mongoose"
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from "@nestjs/mongoose"
+import { User, UserDocument } from "./user.schema"
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec()
+  }
+
+  getHello(): string {
+    return "Hello!"
+  }
+}
