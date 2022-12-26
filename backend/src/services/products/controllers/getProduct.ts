309a46419e47bb589755f3c9d7b9c45e85ed7e4d
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 22:39:48 +0700

    Create controller for getProduct

---
+++ backend/src/services/products/controllers/getProduct.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { Product } from "../product.model.js";
+import mongoose from "mongoose";
+
+export const findProduct = async (shopId: string) => {
+  await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
+
+  const products = await Product.find({ pOwnerId: shopId });
+
+  if (!products) throw new Error("Cannot find product of this shop");
+
+  return products;
+};
+
+export const getProduct = async (req: Request, res: Response) => {
+  try {
+    const { shopId } = req.body;
+
+    const products = await findProduct(shopId);
+
+    return res.json(products);
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).json({ message: error.toString() });
+  }
+};
