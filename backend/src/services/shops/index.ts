41272fcf1ebb77da91153ac471ca1b89f52e2391
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 21:50:01 +0700

    Implement shop register

--- backend/src/services/shops/index.ts
+++ backend/src/services/shops/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
+import { register } from "./controllers/register.js"
 
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.send("Shop route")
 });
+
+router.post("/register", register)
 
 export default router
