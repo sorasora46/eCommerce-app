1fa227058f2a1926e831af74df86149c304ab2f5
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 19:11:54 +0700

    Create shop.model.ts

---
+++ backend/src/services/shops/shop.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+
+interface Shop {
+  shopId: string;
+  sEmail: string;
+  sName: string;
+  sProfileImage?: string;
+}
+
+const shopSchema = new Schema<Shop>({
+  shopId: { type: String, required: true },
+  sEmail: { type: String, required: true },
+  sName: { type: String, required: true },
+  sProfileImage: { type: String, required: false },
+});
+
+export const Shop = model("Shop", shopSchema);
