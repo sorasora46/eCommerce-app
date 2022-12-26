b33408b127f7e93e88d5f39d14afd97e7ca8c296
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 14:15:16 +0700

    Update URI string

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://localhost:27018/"), AuthModule],
+  imports: [MongooseModule.forRoot("mongodb://localhost:27018/eCommerce-app-db"), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
