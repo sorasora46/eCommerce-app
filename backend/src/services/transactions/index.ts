1c1e5d256ef19f801178fd021c64bd2f383574f2
sora-k46 <sorrawit456789@gmail.com>
2022-12-27 02:32:57 +0700

    Create /createtransaction and its controller

--- backend/src/services/transactions/index.ts
+++ backend/src/services/transactions/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
+import { createTransaction } from "./controllers/createTransaction.js"
 
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.send("Transaction route");
 });
+
+router.post("/createtransaction", createTransaction)
 
 export default router;
