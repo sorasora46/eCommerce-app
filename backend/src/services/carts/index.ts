f7bc21522ffab6aed45a4875ec464da00dff9923
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 23:44:17 +0700

    Create route /additem and /getcartitem

--- backend/src/services/carts/index.ts
+++ backend/src/services/carts/index.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
+import { getCartItem } from "./controllers/getCartItem.js";
+import { addItem } from "./controllers/addItem.js";
 
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.send("Cart route");
 });
+
+router.post("/additem", addItem);
+router.get("/getcartitem", getCartItem);
 
 export default router;
