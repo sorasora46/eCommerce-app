0300e2b6703a8e3cc8678df0befd7e61b7277bac
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 21:34:32 +0700

    Implement register

--- backend/src/services/users/index.ts
+++ backend/src/services/users/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
+import { register } from "./controllers/register.js";
 
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("User route")
+  res.send("User route");
 });
 
-export default router
+router.post("/register", register);
+
+export default router;
