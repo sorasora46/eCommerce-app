8b3d68a36f213cc20d8a2bb1f0cb563096338e47
sora-k46 <sorrawit456789@gmail.com>
2022-12-26 14:39:13 +0700

    Format code

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -8,7 +8,7 @@ export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async findAll(): Promise<User[]> {
-    return this.userModel.find().exec()
+    return this.userModel.find().exec();
   }
 
   getHello(): string {
