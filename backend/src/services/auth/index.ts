2761923676a98f4aad316bb66e08873a4eda199d
sora-k46 <sorrawit456789@gmail.com>
2022-12-27 22:44:45 +0700

    Change get method to post method for /login

--- backend/src/services/auth/index.ts
+++ backend/src/services/auth/index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
+router.post("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("login route");
 });
 
