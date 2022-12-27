fafa80c3e89a43c614cf240595c059ce8d70c263
sora-k46 <sorrawit456789@gmail.com>
2022-12-28 01:15:44 +0700

    Implement generating token by jwt

--- backend/src/services/auth/controllers/handleLogin.ts
+++ backend/src/services/auth/controllers/handleLogin.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { authUser } from "../authUser.model.js";
+import { User } from "../../users/user.model.js";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+import jsonwebtoken from "jsonwebtoken";
 
 export const handleLogin = async (req: Request, res: Response) => {
   try {
@@ -14,7 +16,16 @@ export const handleLogin = async (req: Request, res: Response) => {
     if (!(await bcrypt.compare(password, hashedPassword)))
       throw new Error("Incorrect password");
 
-    res.send("login success");
+    const user = await User.findOne({ email: email });
+    const expiration = "12h";
+
+    const accessToken = jsonwebtoken.sign(
+      { ...user },
+      "very secret access_token_secret string",
+      { expiresIn: expiration }
+    );
+
+    res.json({ accessToken: accessToken });
   } catch (error: any) {
     console.log(error);
     res.status(400).json({ error: error.message });
