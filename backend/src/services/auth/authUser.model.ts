abcf938b6a85da38452ebaddb975d004fc489868
sora-k46 <sorrawit456789@gmail.com>
2022-12-27 23:43:56 +0700

    Remove salt from authUser model

--- backend/src/services/auth/authUser.model.ts
+++ backend/src/services/auth/authUser.model.ts
@@ -3,13 +3,11 @@ import { Schema, model } from "mongoose";
 interface authUser {
   email: string;
   hashedPassword: string;
-  salt: string;
 }
 
 const authUserSchema = new Schema<authUser>({
   email: { type: String, required: true },
   hashedPassword: { type: String, required: false },
-  salt: { type: String, required: false },
 });
 
 export const authUser = model("auth", authUserSchema);
