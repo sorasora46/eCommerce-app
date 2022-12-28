87b65a164590357713b70c11ddc820e20eac8ad2
sora-k46 <sorrawit456789@gmail.com>
2022-12-29 01:26:48 +0700

    Implement /refresh

--- backend/src/services/auth/index.ts
+++ backend/src/services/auth/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { handleLogin } from "./controllers/handleLogin.js";
+import { getNewAccessToken } from "./controllers/getRefreshToken.js";
 
 const router = Router();
 
 router.post("/login", handleLogin);
+router.post("/refresh", getNewAccessToken);
 
 export default router;
