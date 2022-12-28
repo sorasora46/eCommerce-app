c89ae5374d9866d0aa956805f3eacd7f29e580d7
sora-k46 <sorrawit456789@gmail.com>
2022-12-29 01:26:13 +0700

    Store refresh token to database

--- backend/src/services/auth/controllers/handleLogin.ts
+++ backend/src/services/auth/controllers/handleLogin.ts
@@ -4,6 +4,7 @@ import { User } from "../../users/user.model.js";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import jsonwebtoken from "jsonwebtoken";
+import { hashedToken } from "../refreshToken.model.js";
 
 export const handleLogin = async (req: Request, res: Response) => {
   try {
@@ -29,6 +30,18 @@ export const handleLogin = async (req: Request, res: Response) => {
       { ...user },
       "very secret refresh_token_secret string"
     );
+
+    const salt = await bcrypt.genSalt();
+    const token = await bcrypt.hash(refreshToken, salt);
+    const result = await hashedToken.create({
+      hashedToken: token,
+      userId: user.userId,
+    });
+
+    result
+      .save()
+      .then()
+      .catch((err) => console.log(err.message));
 
     res.json({ accessToken: accessToken, refreshToken: refreshToken });
   } catch (error: any) {
