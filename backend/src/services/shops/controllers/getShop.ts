33a4eb9cfd1cfbda044ddab1d42a3a0e2d800c80
sora-k46 <sorrawit456789@gmail.com>
2022-12-31 13:28:28 +0700

    Create /getshop route

--- backend/src/services/shops/controllers/getShop.ts
+++ backend/src/services/shops/controllers/getShop.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
+import { Shop } from "../shop.model.js";
 
-export const getShop = (req: Request, res: Response) => {
+export const getShop = async (req: Request, res: Response) => {
   try {
+    // const shop = req.cookies // get shop info from access token from cookies
+    // if (!shop) throw new Error("Not authenticated")
+
+    await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
+
+    const result = await Shop.findOne({})
+    res.json(result)
   } catch (error: any) {
     console.log(error);
     res.json({ error: error.message });
