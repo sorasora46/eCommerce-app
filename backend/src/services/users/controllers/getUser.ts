fbbf864592eb59cc5aed168a4be321cdd04633e3
sora-k46 <sorrawit456789@gmail.com>
2022-12-31 13:28:23 +0700

    Create /getuser route

--- backend/src/services/users/controllers/getUser.ts
+++ backend/src/services/users/controllers/getUser.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
+import { User } from "../user.model.js";
 
-export const getUser = (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
   try {
+    // const user = req.cookies // get user info from access token from cookies
+    // if (!user) throw new Error("Not authenticated")
+
+    await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
+
+    const result = await User.findOne({})
+    res.json(result)
   } catch (error: any) {
     console.log(error);
     res.json({ error: error.message });
