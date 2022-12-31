cf22f1133f858390fc80e017eb2c652efff0001d
sora-k46 <sorrawit456789@gmail.com>
2022-12-31 13:02:02 +0700

    Create form for testing authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+  };
 
   return (
-    <div> HI </div>
-  )
+    <div>
+      <form
+        onSubmit={(e) => {
+          handleSubmit();
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="username">email</label>
+        <input
+          type="email"
+          id="username"
+          required
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <br />
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          id="password"
+          required
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
