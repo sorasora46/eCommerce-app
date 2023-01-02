3c20411155302adb1e4508147d170f29afbf3a06
sora-k46 <sorrawit456789@gmail.com>
2023-01-02 17:03:15 +0700

    Create Routes with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Cart } from "./pages/Cart";
+import { Chat } from "./pages/Chat";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Profile } from "./pages/Profile";
+import { Register } from "./pages/Register";
+import { _404 } from "./pages/_404";
 
 function App() {
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:userId" element={<Profile />} />
+        <Route path="/profile/:userId/cart" element={<Cart />} />
+        <Route path="/chat/:userId" element={<Chat />} />
+        <Route path="*" element={<_404 />} /> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
