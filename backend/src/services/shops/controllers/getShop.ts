c0a2ff8f8a586652fad4bef9e29ad7ad0c3215fd
sora-k46 <sorrawit456789@gmail.com>
2023-01-02 16:13:22 +0700

    Extract userId to find info in DB

--- backend/src/services/shops/controllers/getShop.ts
+++ backend/src/services/shops/controllers/getShop.ts
@@ -7,10 +7,12 @@ export const getShop = async (req: Request, res: Response) => {
     // const shop = req.cookies // get shop info from access token from cookies
     // if (!shop) throw new Error("Not authenticated")
 
+    const { userId } = req.body;
+
     await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
 
-    const result = await Shop.findOne({})
-    res.json(result)
+    const result = await Shop.findOne({ shopId: userId });
+    res.json(result);
   } catch (error: any) {
     console.log(error);
     res.json({ error: error.message });
