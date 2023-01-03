3e0eab8ad5d87f26522a73c09222b479e3e00578
sora-k46 <sorrawit456789@gmail.com>
2023-01-03 23:56:08 +0700

    Create login test

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import axios from "axios";
+
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>login page</div>
+    <div>
+      <form
+        onSubmit={(e) => {
+          axios
+            .post(
+              "http://127.0.0.1:8000/auth/login",
+              { email: email, password: password },
+              { withCredentials: true }
+            )
+            .then((res) => console.log(res))
+            .catch((err) => console.error(err));
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="email">email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button type="submit">login</button>
+      </form>
+    </div>
   );
-}
+};
