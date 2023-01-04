d4263291907abc8ce7826a0603bf2a951a728170
sora-k46 <sorrawit456789@gmail.com>
2023-01-04 14:58:28 +0700

    Validate the tokens

--- backend/src/middlewares/authenticateToken.ts
+++ backend/src/middlewares/authenticateToken.ts
@@ -10,6 +10,20 @@ export const authenticateToken = (
     const access_token = req.cookies.access_token;
     const refresh_token = req.cookies.refresh_token;
 
+    // Redirect to login page when both tokens not found
+    if (!access_token && !refresh_token) {
+      return res.json({
+        redirectUrl: "http://127.0.0.1:5173/login",
+        unathorized: true,
+      });
+    }
+
+    // Get a new access token
+    if (!access_token && refresh_token) {
+      return res.redirect(302, "/auth/refresh");
+    }
+
+    // Verify the token when access token is not expired
     const token = access_token.split(" ")[1];
     jsonwebtoken.verify(
       token,
