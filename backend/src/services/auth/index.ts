a3d79fa3e99aef2689b6b794aa4811f97f90b76b
sora-k46 <sorrawit456789@gmail.com>
2023-01-04 07:25:33 +0700

    Testing redirect request

--- backend/src/services/auth/index.ts
+++ backend/src/services/auth/index.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { handleLogin } from "./controllers/handleLogin.js";
 import { getNewAccessToken } from "./controllers/getRefreshToken.js";
+import { authenticateToken } from "../../middlewares/authenticateToken.js";
 
 const router = Router();
 
 router.post("/login", handleLogin);
-router.post("/refresh", getNewAccessToken);
+router.get("/refresh", getNewAccessToken);
+router.get("/test", authenticateToken, (req, res) => {
+  res.json({
+    token1: req.cookies.access_token,
+    token2: req.cookies.refresh_token,
+  });
+});
 
 export default router;
