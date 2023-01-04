4f45906cea90bf02bf6733c3720b998d72cd34ba
sora-k46 <sorrawit456789@gmail.com>
2023-01-04 16:44:56 +0700

    Testing middleware

--- backend/src/services/auth/index.ts
+++ backend/src/services/auth/index.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { handleLogin } from "./controllers/handleLogin.js";
 import { authenticateToken } from "../../middlewares/authenticateToken.js";
+import { getRefreshToken } from "../../middlewares/getRefreshToken.js";
 
 const router = Router();
 
 router.post("/login", handleLogin);
-router.get("/test", authenticateToken, (req, res) => {
+router.get("/test", getRefreshToken, authenticateToken, (req, res) => {
   res.json({
     token1: req.cookies.access_token,
     token2: req.cookies.refresh_token,
