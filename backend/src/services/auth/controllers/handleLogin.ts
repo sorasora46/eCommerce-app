805294fd7c20f05be0c3a2d6112a2ad271c70f42
sora-k46 <sorrawit456789@gmail.com>
2023-01-04 15:10:22 +0700

    Extract to other functions

--- backend/src/services/auth/controllers/handleLogin.ts
+++ backend/src/services/auth/controllers/handleLogin.ts
@@ -11,43 +11,8 @@ export const handleLogin = async (req: Request, res: Response) => {
     await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
 
     const { email, password } = req.body;
-    const { hashedPassword } = await authUser.findOne({ email: email });
-
-    if (!hashedPassword) throw new Error("Incorrect email");
-    if (!(await bcrypt.compare(password, hashedPassword)))
-      throw new Error("Incorrect password");
-
-    const user = await User.findOne({ email: email }); // user data
-
-    const expiration = "6h"; // expiration of token
-
-    // create access token
-    const accessToken = jsonwebtoken.sign(
-      { ...user },
-      "very secret access_token_secret string",
-      { expiresIn: expiration }
-    );
-
-    // create refresh token
-    const refreshToken = jsonwebtoken.sign(
-      { ...user },
-      "very secret refresh_token_secret string"
-    );
-
-    // save refresh token
-    const salt = await bcrypt.genSalt();
-    const token = await bcrypt.hash(refreshToken, salt);
-    const result = await hashedToken.create({
-      hashedToken: token,
-      userId: user.userId,
     });
 
-    result
-      .save()
-      .then()
-      .catch((err) => console.log(err.message));
-
-    req.user = user;
 
     res
       .cookie("access_token", `Bearer ${accessToken}`, {
