7a7219a50ba7cba060875d0651c7bc6dffe471ca
sora-k46 <sorrawit456789@gmail.com>
2023-01-04 15:10:53 +0700

    Create validateUserData function

--- backend/src/services/auth/controllers/handleLogin.ts
+++ backend/src/services/auth/controllers/handleLogin.ts
@@ -11,6 +11,8 @@ export const handleLogin = async (req: Request, res: Response) => {
     await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
 
     const { email, password } = req.body;
+    validateUserData(email, password).catch((err) => {
+      throw err;
     });
 
 
@@ -29,3 +31,19 @@ export const handleLogin = async (req: Request, res: Response) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+async function validateUserData(
+  email: string,
+  password: string
+): Promise<boolean> {
+  // Find user's hashedPassword by the email
+  const { hashedPassword } = await authUser.findOne({ email: email });
+
+  // Check if the email exist
+  if (!hashedPassword) throw new Error("Incorrect email");
+
+  // Check if the password is correct
+  if (!(await bcrypt.compare(password, hashedPassword)))
+    throw new Error("Incorrect password");
+  return true;
+}
