03930922ae87602231ec0fe14420282afedff63f
sora-k46 <sorrawit456789@gmail.com>
2023-01-04 15:13:51 +0700

    Import missing data

--- backend/src/services/auth/controllers/handleLogin.ts
+++ backend/src/services/auth/controllers/handleLogin.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { User } from "../../users/user.model.js";
 import { authUser } from "../models/authUser.model.js";
+import { IUser, User } from "../../users/user.model.js";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import jsonwebtoken from "jsonwebtoken";
