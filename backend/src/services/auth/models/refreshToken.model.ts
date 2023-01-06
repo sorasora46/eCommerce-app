784f5fe4c2ea0a1dff39fb7bdf1c1831131521fd
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 22:33:43 +0700

    Rename variables

--- backend/src/services/auth/models/refreshToken.model.ts
+++ backend/src/services/auth/models/refreshToken.model.ts
@@ -1,13 +1,13 @@
 import { Schema, model } from "mongoose";
 
-export interface IRefreshToken {
-  hashedToken: string;
+export interface IHashedRefreshToken {
+  hashedRefreshToken: string;
   userId: string;
 }
 
-const refreshTokenSchema = new Schema<IRefreshToken>({
-  hashedToken: { type: String, required: true, unique: true },
+const hashedRefreshTokenSchema = new Schema<IHashedRefreshToken>({
+  hashedRefreshToken: { type: String, required: true, unique: true },
   userId: { type: String, required: true, unique: true },
 });
 
-export const storedRefreshToken = model("Token", refreshTokenSchema);
+export const HashedRefreshToken = model("Token", hashedRefreshTokenSchema);
