c64a85e22f44236862f15a28b54e7c48bf03873e
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 20:46:39 +0700

    Extract name, birth date and profile to ICustomer

--- backend/src/services/users/models/user.model.ts
+++ backend/src/services/users/models/user.model.ts
@@ -3,21 +3,13 @@ import { Schema, model } from "mongoose";
 export interface IUser {
   userId: string;
   email: string;
-  fname: string;
-  lname: string;
-  dateOfBirth: Date;
   role: string;
-  profileImage?: string;
 }
 
 const userSchema = new Schema<IUser>({
   userId: { type: String, required: true },
   email: { type: String, required: true },
-  fname: { type: String, required: true },
-  lname: { type: String, required: true },
-  dateOfBirth: { type: Date, required: true },
-  role: { type: String, required: false },
-  profileImage: { type: String, required: false },
+  role: { type: String, required: true },
 });
 
 export const User = model("User", userSchema);
