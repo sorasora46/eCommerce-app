a2be66707d360276988510947bf198b4ffe13f95
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 23:52:27 +0700

    Add descriptive comments

--- backend/src/services/users/controllers/register.ts
+++ backend/src/services/users/controllers/register.ts
@@ -8,7 +8,10 @@ export const register = async (req: Request, res: Response) => {
     const { email, role } = req.body;
     if (email && role) {
       await createUser(email, role);
-      return res.redirect(307, "/auth/register");
+      return res.redirect(307, "/auth/register"); // be careful with redirect don't mess up again
+      // if set http code wrong, browser will cached the URL
+      // even though you comment the code out it still redirects to that URL
+      // make sure to use http code that has temporary redirect
     }
     return res.status(400).json({ message: "register error" });
   } catch (error: any) {
