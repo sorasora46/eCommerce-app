566b98f0c8be3bf2d7203193942869efd78864fd
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 20:46:56 +0700

    Extend IUser interface

--- backend/src/services/shops/models/shop.model.ts
+++ backend/src/services/shops/models/shop.model.ts
@@ -1,17 +1,14 @@
 import { Schema, model } from "mongoose";
+import { IUser } from "../../users/models/user.model.js";
 
-interface Shop {
-  shopId: string;
-  sEmail: string;
-  sName: string;
-  sProfileImage?: string;
+export interface IShop extends IUser {
+  name: string;
+  profileImage?: string;
 }
 
-const shopSchema = new Schema<Shop>({
-  shopId: { type: String, required: true },
-  sEmail: { type: String, required: true },
-  sName: { type: String, required: true },
-  sProfileImage: { type: String, required: false },
+const shopSchema = new Schema<IShop>({
+  name: { type: String, required: true },
+  profileImage: { type: String },
 });
 
 export const Shop = model("Shop", shopSchema);
