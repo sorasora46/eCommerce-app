f1b027f4920069040c686799fbafaa9aac8a557e
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 23:54:06 +0700

    Test redirect

--- backend/src/services/customers/controllers/register.ts
+++ backend/src/services/customers/controllers/register.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 export const register = (req: Request, res: Response) => {
   try {
+    res.send("Hi from /customer/register");
   } catch (error: any) {
     console.log(error.message);
     res.json({ message: error.message });
