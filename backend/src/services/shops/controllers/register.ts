0ca760b4eb15cb665c0bb5494ff161f72e57d9b0
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 23:54:17 +0700

    Test redirect

--- backend/src/services/shops/controllers/register.ts
+++ backend/src/services/shops/controllers/register.ts
@@ -4,43 +4,44 @@ import { nanoid } from "nanoid";
 import mongoose from "mongoose";
 
 export const register = async (req: Request, res: Response) => {
-  try {
-    const { email, name, profileImage } = req.body;
-    if (email && name) {
-      const result = await createShop(email, name, profileImage);
-      return res.json(result);
-    }
-    return res.status(400).json({ message: "register error" });
-  } catch (error: any) {
-    console.log(error);
-    res.status(400).json({ message: error.message });
-  }
+  res.send("Hi from /shop/register");
+  // try {
+  //   const { email, name, profileImage } = req.body;
+  //   if (email && name) {
+  //     const result = await createShop(email, name, profileImage);
+  //     return res.json(result);
+  //   }
+  //   return res.status(400).json({ message: "register error" });
+  // } catch (error: any) {
+  //   console.log(error);
+  //   res.status(400).json({ message: error.message });
+  // }
 };
 
-export const createShop = async (
-  shop_email: string,
-  shop_name: string,
-  shop_profileImage: string
-) => {
-  await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
+// export const createShop = async (
+//   shop_email: string,
+//   shop_name: string,
+//   shop_profileImage: string
+// ) => {
+//   await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
 
-  const sameEmailShop = await Shop.findOne({ email: shop_email });
-  const sameNameShop = await Shop.findOne({ sName: shop_name });
+//   const sameEmailShop = await Shop.findOne({ email: shop_email });
+//   const sameNameShop = await Shop.findOne({ sName: shop_name });
 
-  if (sameEmailShop) throw new Error("Shop already exist [Email]");
-  if (sameNameShop) throw new Error("Shop already exist [Name]");
+//   if (sameEmailShop) throw new Error("Shop already exist [Email]");
+//   if (sameNameShop) throw new Error("Shop already exist [Name]");
 
-  const newShop = await Shop.create({
-    shopId: nanoid(),
-    sEmail: shop_email,
-    sName: shop_name,
-    sProfileImage: shop_profileImage,
-  });
+//   const newShop = await Shop.create({
+//     shopId: nanoid(),
+//     sEmail: shop_email,
+//     sName: shop_name,
+//     sProfileImage: shop_profileImage,
+//   });
 
-  newShop
-    .save()
-    .then()
-    .catch((err) => console.log(err));
+//   newShop
+//     .save()
+//     .then()
+//     .catch((err) => console.log(err));
 
-  return newShop;
-};
+//   return newShop;
+// };
