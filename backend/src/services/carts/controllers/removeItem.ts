63d3199eb7b6161d06361b37a75e0088cd22722c
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 16:06:54 +0700

    Change models path

--- backend/src/services/carts/controllers/removeItem.ts
+++ backend/src/services/carts/controllers/removeItem.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Cart } from "../cart.model.js";
+import { Cart } from "../models/cart.model.js";
 import mongoose from "mongoose";
 
 export const deleteItem = async (productId: string) => {
--- backend/src/services/products/controllers/removeProduct.ts
+++ backend/src/services/products/controllers/removeProduct.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Product } from "../product.model.js";
+import { Product } from "../models/product.model.js";
 import mongoose from "mongoose";
 
 export const deleteProduct = async (shopId: string, productId: string) => {
