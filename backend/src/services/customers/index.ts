c74f21fbac447fd1982502508aa3e02b784c3dd4
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 21:25:42 +0700

    Import getCustomer controller

--- backend/src/services/customers/index.ts
+++ backend/src/services/customers/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { getCustomer } from "./controllers/getCustomer.js";
 
 const router = express.Router();
 
@@ -6,7 +7,7 @@ router.get("/", (req: Request, res: Response) => {
   res.send("Customer route");
 });
 
-router.get("/getcustomer");
 router.post("register");
+router.get("/getcustomer", getCustomer);
 
 export default router;
