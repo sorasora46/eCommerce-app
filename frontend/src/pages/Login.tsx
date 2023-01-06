d97ecf517fb8ac06427c530f9d98a32fff431274
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 16:27:43 +0700

    Create login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,60 +1,89 @@
 import { useState } from "react";
 import axios from "axios";
+import { CartSVG } from "../components/CartSVG";
+import { Container } from "../components/Container";
+import { Ellipse } from "../components/Ellipse";
+import { Textfield } from "../components/Textfield";
+import { Link } from "react-router-dom";
+import { Button } from "../components/Button";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  function handleLogin() {
+    axios
+      .post(
+        "http://127.0.0.1:8000/auth/login",
+        {
+          email: email,
+          password: password,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        window.location.href = res.data.redirectUrl;
+      })
+      .catch((err) => {
+        console.error(err);
+        // alert("Cannot login");
+      });
+    setEmail("");
+    setPassword("");
+  }
+
   return (
-    <div>
+    <Container
+      width={"100%"}
+      height={"100vh"}
+      className="flex-column"
+      style={{ alignItems: "center", marginTop: "8rem", gap: "5rem" }}
+    >
+      <div id="login-logo">
+        <Link
+          to="/" /** Don't forget to check authentication for protected route */
+        >
+          <Ellipse radius={167} borderSize={4}>
+            <CartSVG />
+          </Ellipse>
+        </Link>
+      </div>
       <form
         onSubmit={(e) => {
-          axios
-            .post(
-              "http://127.0.0.1:8000/auth/login",
-              { email: email, password: password },
-              { withCredentials: true }
-            )
-            .then((res) => {
-              window.location.href = res.data.redirectUrl;
-            })
-            .catch((err) => console.error(err));
-          setEmail("");
-          setPassword("");
+          handleLogin();
           e.preventDefault();
         }}
+        id="username-password"
+        className="container flex-column"
+        style={{ gap: "3rem", alignItems: "center" }}
       >
-        <label htmlFor="email">email</label>
-        <input
+        <Textfield
+          placeholder="Email"
           type="email"
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
           value={email}
+          onChange={setEmail}
+          isRequired={true}
         />
-        <label htmlFor="password">password</label>
-        <input
+        <Textfield
+          placeholder="Password"
           type="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
           value={password}
+          onChange={setPassword}
+          isRequired={true}
         />
-        <button type="submit">login</button>
+        <div
+          id="login-register-buttons"
+          className="container flex-row"
+          style={{ gap: "3rem", marginTop: "20px" }}
+        >
+          <Button className="custom-button" type="submit">
+            Login
+          </Button>
+          <Link to="/register">
+            <Button className="custom-button">Register</Button>
+          </Link>
+        </div>
       </form>
-      <button
-        onClick={() => {
-          axios
-            .get("http://127.0.0.1:8000/auth/test", {
-              withCredentials: true,
-            })
-            .then((res) => {
-              const { unathorized, redirectUrl } = res.data;
-              if (unathorized) window.location.href = redirectUrl;
-              else console.log(res.data);
-            })
-            .catch((err) => console.error(err));
-        }}
-      >
-        test
-      </button>
-    </div>
+    </Container>
   );
 };
