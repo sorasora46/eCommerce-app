e8981009f3cfcfa6915ae34d40638bcaeec85d91
sora-k46 <sorrawit456789@gmail.com>
2023-01-06 23:53:54 +0700

    Change auth user logic and redirect base on role

--- backend/src/services/auth/controllers/register.ts
+++ backend/src/services/auth/controllers/register.ts
@@ -1,36 +1,56 @@
 import { Request, Response } from "express";
 import mongoose from "mongoose";
-import { IAuthUser, AuthUser } from "../../auth/models/authUser.model.js";
+import { AuthUser } from "../../auth/models/authUser.model.js";
 import * as bcrypt from "bcrypt";
+import { UserRole } from "../../users/models/user.model.js";
 
+// TODO: 1. Register AuthUser
+// TODO: 2. Register the user according to the role (need redirect)
 export const register = async (req: Request, res: Response) => {
-  res.send("Hi");
-  // try {
-  //   const { email, role } = req.body;
-  //   if (email && role) {
-  //     const result = await createUser(email, role);
-  //     return res.json(result);
-  //   }
-  //   return res.status(400).json({ message: "register error" });
-  // } catch (error: any) {
-  //   console.log(error);
-  //   res.status(400).json({ message: error.message });
-  // }
+  try {
+    const { email, password, role } = req.body;
+    if (email && password) {
+      // Hash the password and save to DB
+      await createAuthUser(email, password, role);
+
+      // Registation of customer
+      if (role === UserRole.CUSTOMER) {
+        return res.redirect(307, "/customer/register");
+      }
+
+      // Registation of shop owner
+      if (role === UserRole.SHOP) {
+        return res.redirect(307, "/shop/register");
+      }
+    }
+    return res.status(400).json({ message: "register error" });
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
 };
 
-// export const createUser = async (email: string, role: string) => {
-//   await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
+export const createAuthUser = async (
+  email: string,
+  password: string,
+  role: string
+) => {
+  await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
 
-// const salt = await bcrypt.genSalt();
-// const hashedPassword = await bcrypt.hash(user_password, salt);
+  if (UserRole.CUSTOMER !== role && UserRole.SHOP !== role)
+    throw new Error("Role does not exist");
 
-// const newAuthUser = await AuthUser.create({
-//   email: email,
-//   hashedPassword: hashedPassword,
-// });
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-// newAuthUser
-//   .save()
-//   .then()
-//   .catch((err) => console.log(err));
-// };
+  const newAuthUser = await AuthUser.create({
+    email: email,
+    hashedPassword: hashedPassword,
+    role: role,
+  });
+
+  newAuthUser
+    .save()
+    .then()
+    .catch((err) => console.log(err));
+};
