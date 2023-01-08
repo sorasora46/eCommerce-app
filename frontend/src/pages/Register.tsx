08cad74f01a11042fee11e97c7eb9159b4a13c78
sora-k46 <sorrawit456789@gmail.com>
2023-01-08 23:12:20 +0700

    Create state for each input

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { Container } from "../components/Container";
 
 export const Register = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
   function handleRegister() {}
 
   return (
