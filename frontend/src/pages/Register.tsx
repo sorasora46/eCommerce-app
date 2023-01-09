afb86d0a00b03893e35bc0de2c2e0ff7a67a5414
sora-k46 <sorrawit456789@gmail.com>
2023-01-10 00:19:05 +0700

    Render Customer and Shop register inside Tab component

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,7 @@
 import { Container } from "../components/Container";
+import { TabComponent } from "../components/TabComponent";
+import { CustomerRegisterForm } from "../components/login/CustomerRegisterForm";
+import { ShopRegisterForm } from "../components/login/ShopRegisterForm";
 
 export const Register = () => {
   return (
@@ -7,6 +10,10 @@ export const Register = () => {
       height={"100vh"}
       className="flex-column center-items"
     >
+      <TabComponent
+        tabTitles={["Customer", "Shop"]}
+        tabBodies={[<CustomerRegisterForm />, <ShopRegisterForm />]}
+      />
     </Container>
   );
 };
