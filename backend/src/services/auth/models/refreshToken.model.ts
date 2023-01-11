64bab8baa6608dd1dda922f7afda68e10b271ba8
sora-k46 <sorrawit456789@gmail.com>
2023-01-11 21:17:13 +0700

    Add expiration of stored token in mongoDB

--- backend/src/services/auth/models/refreshToken.model.ts
+++ backend/src/services/auth/models/refreshToken.model.ts
@@ -3,11 +3,17 @@ import { Schema, model } from "mongoose";
 export interface IHashedRefreshToken {
   hashedRefreshToken: string;
   userId: string;
+  expireAt?: Date;
 }
 
 const hashedRefreshTokenSchema = new Schema<IHashedRefreshToken>({
   hashedRefreshToken: { type: String, required: true, unique: true },
   userId: { type: String, required: true, unique: true },
+  expireAt: {
+    type: Date,
+    default: new Date(),
+    expires: 60 * 60 * 12,
+  },
 });
 
 export const HashedRefreshToken = model("Token", hashedRefreshTokenSchema);
