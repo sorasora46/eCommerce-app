f2431837646c16e779969f955fcd1026dec5eb39
sora-k46 <sorrawit456789@gmail.com>
2023-01-12 03:34:48 +0700

    Verify the token and redirect the request

--- backend/src/services/users/controllers/getUser.ts
+++ backend/src/services/users/controllers/getUser.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from "express";
+import jsonwebtoken from "jsonwebtoken";
+import { UserRole } from "../models/user.model.js";
 
 export const getUser = async (req: Request, res: Response) => {
   try {
+    const { access_token } = req.signedCookies; // get user info from access token from cookies
+    if (!access_token) throw new Error("Not authenticated");
 
+    const token = access_token.split(" ")[1];
+    jsonwebtoken.verify(
+      token,
+      "very secret access_token_secret string",
+      (err: any, user: any) => {
+        if (err) throw err;
+        if (user._doc.role === UserRole.CUSTOMER)
+          return res.redirect(307, "/customer/getcustomer")
+        if (user._doc.role === UserRole.SHOP)
+          return res.redirect(307, "/shop/getshop")
+      }
+    );
   } catch (error: any) {
     console.log(error);
     res.json({ error: error.message });
