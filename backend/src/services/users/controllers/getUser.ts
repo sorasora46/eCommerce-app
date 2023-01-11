55cd5df102d115651eb87c251a55c16b9487e650
sora-k46 <sorrawit456789@gmail.com>
2023-01-12 03:33:45 +0700

    Remove old logic

--- backend/src/services/users/controllers/getUser.ts
+++ backend/src/services/users/controllers/getUser.ts
@@ -1,18 +1,8 @@
 import { Request, Response } from "express";
-import mongoose from "mongoose";
-import { User } from "../models/user.model.js";
 
 export const getUser = async (req: Request, res: Response) => {
   try {
-    // const user = req.signedCookies // get user info from access token from cookies
-    // if (!user) throw new Error("Not authenticated")
 
-    const { userId } = req.body;
-
-    await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
-
-    const result = await User.findOne({ userId: userId });
-    res.json(result);
   } catch (error: any) {
     console.log(error);
     res.json({ error: error.message });
