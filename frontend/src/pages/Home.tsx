a2d0fabcbd74df59986726c8bd831835113f1a14
sora-k46 <sorrawit456789@gmail.com>
2023-01-12 18:06:17 +0700

    Import SearchBar

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
 import { Navbar } from "../components/navbar/Navbar";
+import { SearchBar } from "../components/SearchBar";
 
 export const Home = () => {
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearch() {
+    axios
+      .get(`http://127.0.0.1:8000/product/search?text=${searchText}`)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div>
       <Navbar />
+      <SearchBar
+        onSubmit={(e) => {
+          handleSearch();
+          setSearchText("");
+          e.preventDefault();
+        }}
+        searchText={searchText}
+        setSearchText={setSearchText}
+      />
     </div>
   );
 };
