768545c017bc0ac4681f2926ae45baa92106299a
sora-k46 <sorrawit456789@gmail.com>
2023-01-12 21:06:19 +0700

    Send top 10 products to render in frontend as init

--- backend/src/services/products/controllers/getProduct.ts
+++ backend/src/services/products/controllers/getProduct.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { Product } from "../models/product.model.js";
 import mongoose from "mongoose";
+import { Shop } from "../../shops/models/shop.model.js";
 
 export const findProduct = async (shopId: string) => {
   const products = await Product.find({ pOwnerId: shopId });
@@ -12,8 +13,14 @@ export const findProduct = async (shopId: string) => {
 
 export const getProduct = async (req: Request, res: Response) => {
   try {
+    await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
 
+    const products = await Product.find({}, null, {
+      sort: { pAmountClick: "desc" },
+      limit: 10,
+    });
 
+    res.json(products);
   } catch (error: any) {
     console.log(error);
     res.status(400).json({ message: error.message });
