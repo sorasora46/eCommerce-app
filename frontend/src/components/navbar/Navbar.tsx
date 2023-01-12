1b53ebdb0f1708f258c853ed2ae905473cdada98
sora-k46 <sorrawit456789@gmail.com>
2023-01-12 16:56:39 +0700

    Extract component to NavbarDesktop

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,47 +1,10 @@
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import { accentColor } from "../../resources/colors";
-import { CartIcon } from "./CartIcon";
-import { ChatIcon } from "./ChatIcon";
-import { Logo } from "./Logo";
-import { LogoutIcon } from "./LogoutIcon";
-import { ProfileIcon } from "./ProfileIcon";
 
 export const Navbar = () => {
   const user = useContext(AuthContext);
 
   return (
-    <div
-      className="container"
-      style={{
-        justifyContent: "space-between",
-        padding: "0 5rem 0 5rem",
-        borderBottom: `2px solid ${accentColor}`,
-        height: "7vh",
-        alignItems: "center",
-      }}
-    >
-      {/* ------------------ Logo ------------------ */}
-      <Logo />
-      {/* ------------------ Logo ------------------ */}
-
-      {/* ------------------ Right Navbar Container ------------------ */}
-      <div className="container" style={{ color: `${accentColor}` }}>
-
-      {/* ------------------ Profile Navbar Container ------------------ */}
-        <ProfileIcon name={user.name} role={user.role} userId={user.userId} />
-      {/* ------------------ Profile Navbar Container ------------------ */}
-
-      {/* ------------------ Menu Navbar Container ------------------ */}
-        <div className="container center-items" style={{ gap: "0.85rem" }}>
-          <ChatIcon userId={user.userId} />
-          <CartIcon userId={user.userId} />
-          <LogoutIcon />
-        </div>
-      {/* ------------------ Menu Navbar Container ------------------ */}
-
       </div>
-      {/* ------------------ Right Navbar Container ------------------ */}
-    </div>
   );
 };
