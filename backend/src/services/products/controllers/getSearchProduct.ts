a70553a566fb1b173053995836cbddb4bb889b2c
sora-k46 <sorrawit456789@gmail.com>
2023-01-12 18:24:38 +0700

    Implement get product by searching text

--- backend/src/services/products/controllers/getSearchProduct.ts
+++ backend/src/services/products/controllers/getSearchProduct.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
+import { Product } from "../models/product.model.js";
 
-export const getSearchProduct = (req: Request, res: Response) => {
-  res.send(req.query)
-}
+export const getSearchProduct = async (req: Request, res: Response) => {
+  try {
+    const search = req.query;
+    const searchPattern = new RegExp(`${search.text}`, "i");
+
+    await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
+    const products = await Product.find({ pName: searchPattern });
+
+    res.send(products);
+  } catch (error: any) {
+    console.log(error.message);
+    res.json({ message: error.message });
+  }
+};
