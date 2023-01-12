cd4e3698b68ed7fd620461bfe902dd69ead32a8b
sora-k46 <sorrawit456789@gmail.com>
2023-01-12 21:05:17 +0700

    Remove old logic

--- backend/src/services/products/controllers/getProduct.ts
+++ backend/src/services/products/controllers/getProduct.ts
@@ -3,8 +3,6 @@ import { Product } from "../models/product.model.js";
 import mongoose from "mongoose";
 
 export const findProduct = async (shopId: string) => {
-  await mongoose.connect("mongodb://localhost:27018/eCommerce-app-db");
-
   const products = await Product.find({ pOwnerId: shopId });
 
   if (!products) throw new Error("Cannot find product of this shop");
@@ -14,11 +12,8 @@ export const findProduct = async (shopId: string) => {
 
 export const getProduct = async (req: Request, res: Response) => {
   try {
-    const { shopId } = req.body;
 
-    const products = await findProduct(shopId);
 
-    return res.json(products);
   } catch (error: any) {
     console.log(error);
     res.status(400).json({ message: error.message });
