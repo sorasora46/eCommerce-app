0f11a03c9107f54f7f16b9d761eb3f56adbe3502
sora-k46 <sorrawit456789@gmail.com>
2023-01-12 18:28:06 +0700

    Format and Remove unused code

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,8 +1,11 @@
-import { CSSProperties, FC, FormEventHandler, useState } from "react";
+import { CSSProperties, FC, FormEventHandler } from "react";
 import { Textfield } from "./Textfield";
 
-export const SearchBar: FC<{ onSubmit: FormEventHandler, searchText: string, setSearchText: Function }> = ({ onSubmit, searchText, setSearchText }) => {
-
+export const SearchBar: FC<{
+  onSubmit: FormEventHandler;
+  searchText: string;
+  setSearchText: Function;
+}> = ({ onSubmit, searchText, setSearchText }) => {
   const style: CSSProperties = {
     width: "100%",
     height: "40vh",
@@ -11,9 +14,16 @@ export const SearchBar: FC<{ onSubmit: FormEventHandler, searchText: string, set
 
   return (
     <div className="container flex-column center-items" style={style}>
-      <h2><label htmlFor="searchbar">Search the product you want</label></h2>
+      <h2>
+        <label htmlFor="searchbar">Search the product you want</label>
+      </h2>
       <form onSubmit={onSubmit}>
-        <Textfield id="searchbar" isRequired value={searchText} onChange={setSearchText} />
+        <Textfield
+          id="searchbar"
+          isRequired
+          value={searchText}
+          onChange={setSearchText}
+        />
       </form>
     </div>
   );
