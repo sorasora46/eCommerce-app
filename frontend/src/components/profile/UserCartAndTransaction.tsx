dadfaa24396656ec7d0c5154b00b870140dd7f1c
sora-k46 <sorrawit456789@gmail.com>
2023-01-16 00:02:16 +0700

    Create UserCartAndTransaction.tsx

---
+++ frontend/src/components/profile/UserCartAndTransaction.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { UserProfileCart } from "./UserProfileCart";
+import { UserProfileTransaction } from "./UserProfileTransaction";
+
+export const UserCartAndTransaction: FC<{ userId: string }> = ({ userId }) => {
+  return (
+    <div
+      className="container flex-row center-items"
+      style={{
+        width: "50%",
+        justifyContent: "space-between",
+      }}
+    >
+      <UserProfileCart userId={userId} />
+      <UserProfileTransaction userId={userId} />
+    </div>
+  );
+};
