55729a7e8830fcd95fed62ab01b600a49853fdca
sora-k46 <sorrawit456789@gmail.com>
2023-01-19 22:50:11 +0700

    Remove outer most style add item detail and buttons

--- frontend/src/components/CartItem.tsx
+++ frontend/src/components/CartItem.tsx
@@ -3,20 +3,10 @@ import { Button } from "./Button";
 import { accentColor, primaryColor } from "../resources/colors";
 import { ProductStatus } from "../resources/productStatus";
 
-export const CartItem: FC<{ cart: any; }> = ({ cart }) => {
+export const CartItem: FC<{ cart: any }> = ({ cart }) => {
   console.log(cart);
   return (
-    <div
-      className="container flex-row"
-      style={{
-        width: "40%",
-        border: `1px solid ${accentColor}`,
-        borderRadius: "10px",
-        alignSelf: "center",
-        backgroundColor: `${accentColor}`,
-        gap: "1rem",
-      }}
-    >
+    <div className="container flex-column center-items shadow cart-item">
       <img
         src={`data:image/*;base64, ${cart?.pImage}`}
         alt={`${cart.pName}`}
@@ -24,36 +14,34 @@ export const CartItem: FC<{ cart: any; }> = ({ cart }) => {
           objectFit: "cover",
           border: `1px solid ${accentColor}`,
           borderRadius: "10px",
+          minHeight: "150px",
+          minWidth: "150px",
         }}
-        width="150rem"
-        height="150rem" />
+        width="150px"
+        height="150px"
+      />
       <div
         className="container flex-column"
-        style={{ color: `${primaryColor}`, padding: "1rem 0 1rem 0" }}
+        style={{ color: `${primaryColor}` }}
       >
-        <h3>{cart.pName}</h3>
-        <p>Price: {cart.pPrice}</p>
-        <p>Amount: {cart.productAmount}</p>
-        <p>Status: {cart.status}</p>
-        <p>
-          From: {cart.shopName} (Shop ID: {cart.shopId})
-        </p>
+        <div style={{ paddingBottom: "1rem" }}>
+          <h3>{cart.pName}</h3>
+          <p>Price: {cart.pPrice}</p>
+          <p>Amount: {cart.productAmount}</p>
+          <p>Status: {cart.status}</p>
+          <p>From: {cart.shopName}</p>
+          <p>(Shop ID: {cart.shopId})</p>
+        </div>
+        {cart.status === ProductStatus.waitingPayment && (
+          <Button style={{ alignSelf: "center" }}>Buy now</Button>
+        )}
+        {cart.status === ProductStatus.shipping && (
+          <Button style={{ alignSelf: "center" }}>Cancel Order</Button>
+        )}
+        {cart.status === ProductStatus.waitingRecieved && (
+          <Button style={{ alignSelf: "center" }}>Confirm Recieved</Button>
+        )}
       </div>
-      {cart.status === ProductStatus.waitingPayment ? (
-        <Button style={{ alignSelf: "center" }}>Buy now</Button>
-      ) : (
-        ""
-      )}
-      {cart.status === ProductStatus.shipping ? (
-        <Button style={{ alignSelf: "center" }}>Cancel Order</Button>
-      ) : (
-        ""
-      )}
-      {cart.status === ProductStatus.waitingRecieved ? (
-        <Button style={{ alignSelf: "center" }}>Confirm</Button>
-      ) : (
-        ""
-      )}
     </div>
   );
 };
