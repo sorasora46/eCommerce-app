dabef7404512c4f46a28ba666dd9390c3ae0c4ed
sora-k46 <sorrawit456789@gmail.com>
2023-01-20 23:02:35 +0700

    Extract style to css file

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -9,33 +9,17 @@ export const Modal: FC<{
 }> = ({ isOpen, setIsOpen, onClose, children }) => {
   const outerStyle: CSSProperties = {
     display: `${isOpen ? "flex" : "none"}`,
-    position: "fixed", // it stay there, ignore the scrolling
-    top: 0,
-    left: 0,
-    zIndex: 2,
-    width: "100vw",
-    height: "100vh",
-    backdropFilter: "brightness(0.6) blur(2px)",
-  };
-
-  const innerStyle: CSSProperties = {
-    width: "50%",
-    height: "50vh",
-    border: `1px solid ${accentColor}`,
-    borderRadius: "10px",
-    backgroundColor: `${primaryColor}`,
   };
 
   return (
     <div
       onClick={() => setIsOpen(false)}
       style={outerStyle}
-      className="container center-items"
+      className="container center-items product-modal-outer"
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="container center-items"
-        style={innerStyle}
+        className="container center-items product-modal-inner"
       >
         {children}
       </div>
