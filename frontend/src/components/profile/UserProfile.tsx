6fba6a6d5339d719558ceb0209e1578029b2f545
sora-k46 <sorrawit456789@gmail.com>
2023-01-21 15:47:54 +0700

    Add isSameProfile prop and icons for editing email and name

--- frontend/src/components/profile/UserProfile.tsx
+++ frontend/src/components/profile/UserProfile.tsx
@@ -1,7 +1,21 @@
 import { FC } from "react";
+import { MdEditCalendar, MdOutlineEdit } from "react-icons/md";
 import { primaryColor } from "../../resources/colors";
 
-export const UserProfile: FC<{ user: any }> = ({ user }) => {
+export const UserProfile: FC<{ user: any; isSameProfile: boolean }> = ({
+  user,
+  isSameProfile,
+}) => {
+  const name =
+    user?.role === "CUSTOMER"
+      ? `${user.name?.fname} ${user.name?.lname}`
+      : user?.role === "SHOP"
+      ? `${user?.name}`
+      : "";
+  // TODO: Changable profile image
+  // TODO: Make name and email editable
+  // TODO: Make birthday editable
+
   return (
     <div
       className="container flex-column"
@@ -21,7 +35,7 @@ export const UserProfile: FC<{ user: any }> = ({ user }) => {
       >
         <img
           src={`data:image/*;base64, ${user?.profileImage}`}
-          alt={`${user?.name}`}
+          alt={`${name}'s profile image'`}
           style={{ objectFit: "cover", borderRadius: "50%" }}
           width="100%"
           height="100%"
@@ -32,19 +46,24 @@ export const UserProfile: FC<{ user: any }> = ({ user }) => {
         style={{ color: `${primaryColor}` }}
       >
         <div className="container-flex-colunm">
-          {user?.role === "CUSTOMER" ? (
-            <h2 style={{ textAlign: "center" }}>
-              {user?.name.fname} {user?.name.lname}
-            </h2>
-          ) : user?.role === "SHOP" ? (
-            <h2>Shop's name: {user?.name}</h2>
-          ) : (
-            ""
-          )}
+          <h2 style={{ textAlign: "center" }}>
+            {name}
+            {isSameProfile && <MdOutlineEdit style={{ marginLeft: "1rem" }} />}
+          </h2>
         </div>
         <div className="container-flex-column" style={{ textAlign: "center" }}>
-          <h2>Email: {user?.email}</h2>
-          <h2>Birthdate: {new Date(user?.dateOfBirth).toDateString()}</h2>
+          <h2>
+            Email: {user?.email}
+            {isSameProfile && <MdOutlineEdit style={{ marginLeft: "1rem" }} />}
+          </h2>
+          {user?.role === "CUSTOMER" && (
+            <h2>
+              Birthdate: {new Date(user?.dateOfBirth).toDateString()}
+              {isSameProfile && (
+                <MdEditCalendar style={{ marginLeft: "1rem" }} />
+              )}
+            </h2>
+          )}
         </div>
       </div>
     </div>
