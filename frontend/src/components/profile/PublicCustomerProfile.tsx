19b1316b2749699e1162845fbd06e2d234c9f475
sora-k46 <sorrawit456789@gmail.com>
2023-01-26 23:45:11 +0700

    Update navigate to userContext.userId

--- frontend/src/components/profile/PublicCustomerProfile.tsx
+++ frontend/src/components/profile/PublicCustomerProfile.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import {
   MdOutlinePersonAddAlt,
   MdMessage,
@@ -6,9 +7,11 @@ import {
   MdOutlinePersonOff,
 } from "react-icons/md";
 import { useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 // TODO: Add functionality to each of menu
 export const PublicCustomerProfile = () => {
   const { userId } = useParams();
+  const userContext = useContext(AuthContext);
   const navigate = useNavigate();
 
   return (
@@ -16,7 +19,7 @@ export const PublicCustomerProfile = () => {
       <p>customer profile of {userId}</p>
       <br />
       <p onClick={() => {
-        navigate(`/chat/${userId}`)
+        navigate(`/chat/${userContext.userId}?with=${userId}`)
       }}>
         Chat <MdMessage />
       </p>
