0378d661fccfaac3f8b69a86fe4e2f2c85164efd
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 21:18:41 +0700

    [Rewrite Backend] - Create customer model

---
+++ backend/src/services/t_customer/models/customer.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+export interface ICustomer {
+  userId: string;
+  email: string;
+  name: {
+    fname: string;
+    lname: string;
+  };
+  dateOfBirth: Date;
+  profileImage: Buffer;
+}
+
+const customerSchema = new Schema<ICustomer>({
+  userId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  name: {
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+  },
+  dateOfBirth: { type: Date, required: true },
+  profileImage: { type: Buffer, required: true },
+});
+
+export const Customer = model("Customer", customerSchema);
