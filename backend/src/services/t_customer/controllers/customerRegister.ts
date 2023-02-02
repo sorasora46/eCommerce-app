bba005a2ac3e4747cea06ef6bbe12e82dc74d7ce
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 23:39:07 +0700

    [Rewrite Backend] - Format code

--- backend/src/services/t_customer/controllers/customerRegister.ts
+++ backend/src/services/t_customer/controllers/customerRegister.ts
@@ -6,20 +6,25 @@ export default function customerRegister(req: Request, res: Response) {
   try {
     const { userId, email, fname, lname, dateOfBirth } = req.body;
     const imageBuffer = req.file.buffer;
+
     const customerAge = calculateAge(new Date(dateOfBirth));
     if (customerAge < 16) throw new Error("age require atleast 16 years old");
-    Customer.create({
-      userId: userId,
-      email: email,
-      fname: fname,
-      lname: lname,
-      dateOfBirth: new Date(dateOfBirth),
-      profileImage: imageBuffer
-    }, (err: any, result: any) => {
-      if (err) return res.send(err.message)
-      console.log(result);
-      res.send(result)
-    })
+
+    Customer.create(
+      {
+        userId: userId,
+        email: email,
+        fname: fname,
+        lname: lname,
+        dateOfBirth: new Date(dateOfBirth),
+        profileImage: imageBuffer,
+      },
+      (err: any, result: any) => {
+        if (err) return res.send(err.message);
+        console.log(result);
+        res.send(result);
+      }
+    );
   } catch (err: any) {
     console.log(err.message);
     res.send(err.message);
