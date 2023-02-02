ba91ece3e854f8107e7f6bf2adbd7a1f89b6f32d
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 22:13:24 +0700

    [Rewrite Backend] - Add find shop by Id

--- backend/src/services/t_shop/controllers/getShopById.ts
+++ backend/src/services/t_shop/controllers/getShopById.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
+import { Shop } from "../models/shop.model.js";
 
 export default function getShopById(req: Request, res: Response) {
-  res.send("hi");
+  try {
+    const { userId } = req.params;
+    Shop.findOne({ userId: userId }, (err: any, customer: any) => {
+      if (err) throw err;
+      console.log(customer);
+      res.send(customer);
+    });
+  } catch (err: any) {
+    console.log(err.message);
+    res.send(err.message);
+  }
 }
