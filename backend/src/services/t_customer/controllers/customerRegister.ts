6d0845fe85d3ed4c8ce5232f3796618a8030c742
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 23:37:07 +0700

    [Rewrite Backend] - Calculate customer age

--- backend/src/services/t_customer/controllers/customerRegister.ts
+++ backend/src/services/t_customer/controllers/customerRegister.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
+import { calculateAge } from "../../../helpers/calculateAge.js";
 import { Customer } from "../models/customer.model.js";
 
 export default function customerRegister(req: Request, res: Response) {
   try {
     const { userId, email, fname, lname, dateOfBirth } = req.body;
     const imageBuffer = req.file.buffer;
+    const customerAge = calculateAge(new Date(dateOfBirth));
+    if (customerAge < 16) throw new Error("age require atleast 16 years old");
     Customer.create({
       userId: userId,
       email: email,
