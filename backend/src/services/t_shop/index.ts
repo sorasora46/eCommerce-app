4986f7b2bdc169e44e9e3e4eb7a5b4bbaceaee2a
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 22:06:59 +0700

    [Rewrite Backend] - Add file uploader middleware

--- backend/src/services/t_shop/index.ts
+++ backend/src/services/t_shop/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import getShopById from "./controllers/getShopById.js";
 import shopRegister from "./controllers/shopRegister.js";
+import multer from "multer";
 
 const router = Router();
+const upload = multer();
 
-router.post("/register", shopRegister);
+router.post("/register", upload.single("profileImage"), shopRegister);
 router.get("/:userId", getShopById);
 
 export default router;
