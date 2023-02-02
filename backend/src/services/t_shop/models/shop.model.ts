0a24f64c8514b4025e6c8a44af8283fe35228d10
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 21:21:13 +0700

    [Rewrite Backend] - Create shop model

---
+++ backend/src/services/t_shop/models/shop.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+
+export interface IShop {
+  userId: string;
+  email: string;
+  name: string;
+  profileImage: Buffer;
+}
+
+const shopSchema = new Schema<IShop>({
+  userId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
+  profileImage: { type: Buffer, required: true },
+});
+
+export const Shop = model("Shop", shopSchema);
