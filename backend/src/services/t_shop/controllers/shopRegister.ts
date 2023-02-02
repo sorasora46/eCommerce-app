7be1e578296c7d54162ca398ed8b78464004ecbc
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 23:40:50 +0700

    [Rewrite Backend] - Format code

--- backend/src/services/t_shop/controllers/shopRegister.ts
+++ backend/src/services/t_shop/controllers/shopRegister.ts
@@ -4,17 +4,20 @@ import { Shop } from "../models/shop.model.js";
 export default function shopRegister(req: Request, res: Response) {
   try {
     const { userId, email, name } = req.body;
-    const imageBuffer = req.file.buffer
-    Shop.create({
-      userId: userId,
-      email: email,
-      name: name,
-      profileImage: imageBuffer
-    }, (err: any, result: any) => {
-      if (err) return res.send(err.message)
-      console.log(result);
-      res.send(result)
-    })
+    const imageBuffer = req.file.buffer;
+    Shop.create(
+      {
+        userId: userId,
+        email: email,
+        name: name,
+        profileImage: imageBuffer,
+      },
+      (err: any, result: any) => {
+        if (err) return res.send(err.message);
+        console.log(result);
+        res.send(result);
+      }
+    );
   } catch (err: any) {
     console.log(err.message);
     res.send(err.message);
