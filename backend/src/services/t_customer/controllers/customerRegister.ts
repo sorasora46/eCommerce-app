2ff5302954a3b8c7ea486107c239ac00650ef733
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 23:36:41 +0700

    [Rewrite Backend] - Extract name to fname, lname

--- backend/src/services/t_customer/controllers/customerRegister.ts
+++ backend/src/services/t_customer/controllers/customerRegister.ts
@@ -3,16 +3,13 @@ import { Customer } from "../models/customer.model.js";
 
 export default function customerRegister(req: Request, res: Response) {
   try {
-    const { userId, email, name, dateOfBirth } = req.body;
-    const { fname, lname } = JSON.parse(name);
-    const imageBuffer = req.file.buffer
+    const { userId, email, fname, lname, dateOfBirth } = req.body;
+    const imageBuffer = req.file.buffer;
     Customer.create({
       userId: userId,
       email: email,
-      name: {
-        fname: fname,
-        lname: lname,
-      },
+      fname: fname,
+      lname: lname,
       dateOfBirth: new Date(dateOfBirth),
       profileImage: imageBuffer
     }, (err: any, result: any) => {
