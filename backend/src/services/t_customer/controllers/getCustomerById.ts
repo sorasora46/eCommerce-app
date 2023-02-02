e71d35e9d0c887fe1baf5886e01c8c544e9c02c9
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 22:04:44 +0700

    [Rewrite Backend] - Add get customer by Id

--- backend/src/services/t_customer/controllers/getCustomerById.ts
+++ backend/src/services/t_customer/controllers/getCustomerById.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
+import { Customer } from "../models/customer.model.js";
 
 export default function getCustomerById(req: Request, res: Response) {
-  res.send("hi");
+  try {
+    const { userId } = req.params;
+    Customer.findOne({ userId: userId }, (err: any, customer: any) => {
+      if (err) throw err;
+      console.log(customer);
+      res.send(customer);
+    });
+  } catch (err: any) {
+    console.log(err.message);
+    res.send(err.message);
+  }
 }
