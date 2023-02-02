c8fe4e5deddbabed797012733b16e76a730a9f4f
sora-k46 <sorrawit456789@gmail.com>
2023-02-02 22:04:32 +0700

    [Rewrite Backend] - Add create customer to database

--- backend/src/services/t_customer/controllers/customerRegister.ts
+++ backend/src/services/t_customer/controllers/customerRegister.ts
@@ -1,5 +1,27 @@
 import { Request, Response } from "express";
+import { Customer } from "../models/customer.model.js";
 
 export default function customerRegister(req: Request, res: Response) {
-  res.send("hi");
+  try {
+    const { userId, email, name, dateOfBirth } = req.body;
+    const { fname, lname } = JSON.parse(name);
+    const imageBuffer = req.file.buffer
+    Customer.create({
+      userId: userId,
+      email: email,
+      name: {
+        fname: fname,
+        lname: lname,
+      },
+      dateOfBirth: new Date(dateOfBirth),
+      profileImage: imageBuffer
+    }, (err: any, result: any) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(result)
+    })
+  } catch (err: any) {
+    console.log(err.message);
+    res.send(err.message);
+  }
 }
