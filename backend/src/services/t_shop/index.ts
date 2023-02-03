db55c5c537eaff017fda77f62f78843dae752e00
sora-k46 <sorrawit456789@gmail.com>
2023-02-04 01:30:07 +0700

    [Rewrite Backend] - Add delete shop api endpoint

--- backend/src/services/t_shop/index.ts
+++ backend/src/services/t_shop/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import getShopById from "./controllers/getShopById.js";
 import shopRegister from "./controllers/shopRegister.js";
 import multer from "multer";
+import deleteShopById from "./controllers/deleteShopById.js";
 
 const router = Router();
 const upload = multer();
 
 router.post("/register", upload.single("profileImage"), shopRegister);
 router.get("/:userId", getShopById);
+router.delete("/:userId/delete", deleteShopById);
 
 export default router;
