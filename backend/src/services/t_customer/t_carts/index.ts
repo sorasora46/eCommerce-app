9daf79be58782e71587e4e91d207038c7a403906
sora-k46 <sorrawit456789@gmail.com>
2023-02-04 01:41:26 +0700

    [Rewrite Backend] - Remove file uploader middleware

--- backend/src/services/t_customer/t_carts/index.ts
+++ backend/src/services/t_customer/t_carts/index.ts
@@ -2,13 +2,10 @@ import { Router } from "express";
 import addProductToCart from "./controllers/addProductToCart.js";
 import getProductsInCart from "./controllers/getProductsInCart.js";
 import removeProductFromCart from "./controllers/removeProductFromCart.js";
-import multer from "multer";
 
 const router = Router({ mergeParams: true });
-const upload = multer();
 
 router.get("/", getProductsInCart);
-router.post("/addproduct", upload.single("productImage"), addProductToCart);
 router.delete("/remove/:productId", removeProductFromCart);
 
 export default router;
