0c533a180341cc4966ebf5f623f82b0f23b8be13
sora-k46 <sorrawit456789@gmail.com>
2023-02-04 01:40:08 +0700

    [Rewrite Backend] - Create updateProductInCart.ts

---
+++ backend/src/services/t_customer/t_carts/controllers/updateProductInCart.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { Cart, IUpdateCart } from "../models/cart.model.js";
+
+export default function updateProductInCart(req: Request, res: Response) {
+  try {
+    const { userId, productId } = req.params;
+    const { status, productAmount } = req.body;
+
+    const updatedData: IUpdateCart = {};
+    if (status) updatedData.status = status;
+    if (productAmount) updatedData.productAmount = productAmount;
+
+    Cart.findOneAndUpdate(
+      { userId: userId, productId: productId },
+      updatedData,
+      { new: true },
+      (err: any, result: any) => {
+        if (err) return res.send(err.message);
+        res.send(result);
+      }
+    );
+  } catch (err: any) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+}
