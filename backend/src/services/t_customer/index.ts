852e3508a4da7b0cc43474aacb32f976e4a806ea
sora-k46 <sorrawit456789@gmail.com>
2023-02-04 01:29:33 +0700

    [Rewrite Backend] - Add delete customer api endpoint

--- backend/src/services/t_customer/index.ts
+++ backend/src/services/t_customer/index.ts
@@ -3,12 +3,14 @@ import customerRegister from "./controllers/customerRegister.js";
 import getCustomerById from "./controllers/getCustomerById.js";
 import _cartRoute from "./t_carts/index.js";
 import multer from "multer";
+import deleteCustomerById from "./controllers/deleteCustomerById.js";
 
 const router = Router();
 const upload = multer();
 
 router.post("/register", upload.single("profileImage"), customerRegister);
 router.use("/:userId/cart", _cartRoute);
+router.delete("/:userId/delete", deleteCustomerById);
 router.get("/:userId", getCustomerById);
 
 export default router;
