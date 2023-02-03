da6657bd937b0a6102536e03dd9b73566938a79c
sora-k46 <sorrawit456789@gmail.com>
2023-02-04 01:39:53 +0700

    [Rewrite Backend] - Create clearCart.ts

---
+++ backend/src/services/t_customer/t_carts/controllers/clearCart.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { Cart } from "../models/cart.model.js";
+
+export default function clearCart(req: Request, res: Response) {
+  try {
+    const { userId } = req.params;
+    Cart.deleteMany({ userId: userId }, (err: any, result: any) => {
+      if (err) return res.send(err.message);
+      res.send(err.message);
+    });
+  } catch (err: any) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+}
