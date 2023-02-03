c1076f3448cc13238882588a4a3b1aa3a2889043
sora-k46 <sorrawit456789@gmail.com>
2023-02-03 10:25:55 +0700

    [Rewrite Backend] - Create getProducts.ts

---
+++ backend/src/services/t_products/controllers/getProducts.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { Product } from "../models/product.model.js";
+
+export default function getProducts(req: Request, res: Response) {
+  try {
+    const { userId } = req.params;
+    Product.find({ productOwner: userId }, (err: any, products: any) => {
+      if (err) return res.send(err.message);
+      console.log(products);
+      res.send(products);
+    });
+  } catch (err: any) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+}
