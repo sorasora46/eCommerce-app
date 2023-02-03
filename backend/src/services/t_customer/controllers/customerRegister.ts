bfa3e9e78617280cab3fb0c649db7535299ab183
sora-k46 <sorrawit456789@gmail.com>
2023-02-03 13:04:20 +0700

    [Rewrite Backend] - Simplify parsing arguments

--- backend/src/services/t_customer/controllers/customerRegister.ts
+++ backend/src/services/t_customer/controllers/customerRegister.ts
@@ -5,19 +5,19 @@ import { Customer } from "../models/customer.model.js";
 export default function customerRegister(req: Request, res: Response) {
   try {
     const { userId, email, fname, lname, dateOfBirth } = req.body;
-    const imageBuffer = req.file.buffer;
+    const profileImage = req.file.buffer;
 
     const customerAge = calculateAge(new Date(dateOfBirth));
     if (customerAge < 16) throw new Error("age require atleast 16 years old");
 
     Customer.create(
       {
-        userId: userId,
-        email: email,
-        fname: fname,
-        lname: lname,
-        dateOfBirth: new Date(dateOfBirth),
-        profileImage: imageBuffer,
+        userId,
+        email,
+        fname,
+        lname,
+        dateOfBirth,
+        profileImage,
       },
       (err: any, result: any) => {
         if (err) return res.send(err.message);
