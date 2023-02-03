6ee6bc23e598facf486140a8fbbca8ac88aa904c
sora-k46 <sorrawit456789@gmail.com>
2023-02-04 01:40:50 +0700

    [Rewrite Backend] - Remove unnecessary attributes

--- backend/src/services/t_customer/t_carts/controllers/addProductToCart.ts
+++ backend/src/services/t_customer/t_carts/controllers/addProductToCart.ts
@@ -1,31 +1,18 @@
 import { Request, Response } from "express";
-import { Cart } from "../models/cart.model.js";
+import { Cart, CartProductStatus } from "../models/cart.model.js";
 
 export default function addProductToCart(req: Request, res: Response) {
   try {
-    const {
-      status,
-      shopName,
-      shopId,
-      productId,
-      productAmount,
-      productName,
-      productPrice,
-    } = req.body;
-    const productImage = req.file.buffer;
-    const { userId } = req.params;
+    const { productAmount } = req.body;
+    const { userId, productId } = req.params;
+    const status = CartProductStatus.waiting;
 
     Cart.create(
       {
         userId,
-        shopName,
-        shopId,
+        status,
         productId,
         productAmount,
-        productName,
-        productPrice,
-        status,
-        productImage,
       },
       (err: any, result: any) => {
         if (err) return res.send(err.message);
