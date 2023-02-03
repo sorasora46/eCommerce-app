416ddbb8f959702813e6dd239f6e6136d9d93f9d
sora-k46 <sorrawit456789@gmail.com>
2023-02-03 13:01:40 +0700

    [Rewrite Backend] - Create cart model

---
+++ backend/src/services/t_customer/t_carts/models/cart.model.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+export interface ICart {
+  userId: string;
+  status: string;
+  shopName: string;
+  shopId: string;
+  productId: string;
+  productAmount: number;
+  productName: string;
+  productPrice: number;
+  productImage: Buffer;
+}
+
+const cartSchema = new Schema<ICart>({
+  userId: { type: String, required: true },
+  productId: { type: String, required: true },
+  status: { type: String, required: true }, // waiting for payment, shipping, waiting for recieved confirmation
+  productAmount: { type: Number, required: true },
+  productName: { type: String, required: true },
+  productPrice: { type: Number, required: true },
+  shopName: { type: String, required: true },
+  shopId: { type: String, required: true },
+  productImage: { type: Buffer, required: true },
+});
+
+export const Cart = model("Cart", cartSchema);
