967d6ab66be32f1b98520ec4f0a1147af7c70563
sora-k46 <sorrawit456789@gmail.com>
2023-02-04 01:42:02 +0700

    [Rewrite Backend] - Add endpoint for clearCart, updateCart

--- backend/src/services/t_customer/t_carts/index.ts
+++ backend/src/services/t_customer/t_carts/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import addProductToCart from "./controllers/addProductToCart.js";
+import clearCart from "./controllers/clearCart.js";
 import getProductsInCart from "./controllers/getProductsInCart.js";
 import removeProductFromCart from "./controllers/removeProductFromCart.js";
+import updateProductInCart from "./controllers/updateProductInCart.js";
 
 const router = Router({ mergeParams: true });
 
 router.get("/", getProductsInCart);
+router.delete("/clearcart", clearCart);
 router.delete("/remove/:productId", removeProductFromCart);
+router.put("/update/:productId", updateProductInCart);
 
 export default router;
