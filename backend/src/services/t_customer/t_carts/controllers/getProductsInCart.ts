0db0756dd7b4be7cb30a08c0b22594edf8c158c2
sora-k46 <sorrawit456789@gmail.com>
2023-02-05 23:26:17 +0700

    [Rewrite Backend] - Return a joined collection

--- backend/src/services/t_customer/t_carts/controllers/getProductsInCart.ts
+++ backend/src/services/t_customer/t_carts/controllers/getProductsInCart.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from "express";
+import { Product } from "../../../t_products/models/product.model.js";
 import { Cart } from "../models/cart.model.js";
 
 export default function getProductsInCart(req: Request, res: Response) {
   try {
     const { userId } = req.params;
-    Cart.find({ userId: userId }, (err: any, cart: any) => {
+    Cart.find({ userId: userId }, async (err: any, cart: any) => {
       if (err) return res.send(err.message);
-      res.send(cart);
+
+      const joinedData = [];
+      for (const item of cart) {
+        const product = await Product.findOne({ productId: item.productId });
+        if (!product) continue;
+
+        joinedData.push({
+          userId: item.userId,
+          status: item.status,
+          productId: item.productId,
+          productAmount: item.productAmount,
+          productName: product.productName,
+          productPrice: product.productPrice,
+          productOwner: product.productOwner,
+          productImage: product.productImage,
+        });
+      }
+
+      res.send(joinedData);
     });
   } catch (err: any) {
     console.log(err.message);
