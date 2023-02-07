3f7a19be9f32a0393b49b9d711d2e29bca49553c
sora-k46 <sorrawit456789@gmail.com>
2023-02-07 19:36:31 +0700

    [Rewrite Backend] - Fix update bugs

--- backend/src/services/t_user/t_customer/controllers/updateCustomerById.ts
+++ backend/src/services/t_user/t_customer/controllers/updateCustomerById.ts
@@ -11,7 +11,48 @@ export default function updateCustomerById(req: Request, res: Response) {
     const updatedData: IUpdateCustomer = {};
     if (email) {
       updatedData.email = email;
+
       User.findOneAndUpdate(
+        { userId: userId },
+        updatedData,
+        { new: true },
+        async (err: any, userResult: any) => {
+          if (err) return res.send(err.message);
+
+          const customerResultEmail = await Customer.findOneAndUpdate(
+            { userId: userId },
+            updatedData,
+            { new: true }
+          );
+
+          if (fname || lname || dateOfBirth || profileImage) {
+            if (fname) updatedData.fname = fname;
+            if (lname) updatedData.lname = lname;
+            if (dateOfBirth) updatedData.dateOfBirth = dateOfBirth;
+            if (profileImage) updatedData.profileImage = profileImage;
+
+            Customer.findOneAndUpdate(
+              { userId: userId },
+              updatedData,
+              { new: true },
+              (err: any, customerResult: any) => {
+                if (err) return res.send(err.message);
+                res.send({ user: userResult, customer: customerResult });
+              }
+            );
+            return;
+          }
+
+          res.send({ user: userResult, customer: customerResultEmail });
+        }
+      );
+    } else {
+      if (fname) updatedData.fname = fname;
+      if (lname) updatedData.lname = lname;
+      if (dateOfBirth) updatedData.dateOfBirth = dateOfBirth;
+      if (profileImage) updatedData.profileImage = profileImage;
+
+      Customer.findOneAndUpdate(
         { userId: userId },
         updatedData,
         { new: true },
@@ -21,21 +62,6 @@ export default function updateCustomerById(req: Request, res: Response) {
         }
       );
     }
-
-    if (fname) updatedData.fname = fname;
-    if (lname) updatedData.lname = lname;
-    if (dateOfBirth) updatedData.dateOfBirth = dateOfBirth;
-    if (profileImage) updatedData.profileImage = profileImage;
-
-    Customer.findOneAndUpdate(
-      { userId: userId },
-      updatedData,
-      { new: true },
-      (err: any, result: any) => {
-        if (err) return res.send(err.message);
-        res.send(result);
-      }
-    );
   } catch (err: any) {
     console.log(err.message);
     res.send(err.message);
--- backend/src/services/t_user/t_shop/controllers/updateShopById.ts
+++ backend/src/services/t_user/t_shop/controllers/updateShopById.ts
@@ -11,7 +11,43 @@ export default function updateShopById(req: Request, res: Response) {
     const updatedData: IUpdateShop = {};
     if (email) {
       updatedData.email = email;
+
       User.findOneAndUpdate(
+        { userId: userId },
+        updatedData,
+        { new: true },
+        async (err: any, userResult: any) => {
+          if (err) return res.send(err.message);
+
+          const shopResultEmail = await Shop.findOneAndUpdate(
+            { userId: userId },
+            updatedData,
+            { new: true }
+          );
+
+          if (name || profileImage) {
+            if (name) updatedData.name = name;
+            if (profileImage) updatedData.profileImage = profileImage;
+
+            Shop.findOneAndUpdate(
+              { userId: userId },
+              updatedData,
+              { new: true },
+              (err: any, shopResult: any) => {
+                if (err) return res.send(err.message);
+                res.send({ user: userResult, shop: shopResult });
+              }
+            );
+            return;
+          }
+
+          res.send({ user: userResult, shop: shopResultEmail });
+        }
+      );
+    } else {
+      if (name) updatedData.name = name;
+      if (profileImage) updatedData.profileImage = profileImage;
+      Shop.findOneAndUpdate(
         { userId: userId },
         updatedData,
         { new: true },
@@ -21,18 +57,6 @@ export default function updateShopById(req: Request, res: Response) {
         }
       );
     }
-    if (name) updatedData.name = name;
-    if (profileImage) updatedData.profileImage = profileImage;
-
-    Shop.findOneAndUpdate(
-      { userId: userId },
-      updatedData,
-      { new: true },
-      (err: any, result: any) => {
-        if (err) return res.send(err.message);
-        res.send(result);
-      }
-    );
   } catch (err: any) {
     console.log(err.message);
     res.send(err.message);
