a498d40c04ac81a779c7abd98b4bcb9a9faba24d
sora-k46 <sorrawit456789@gmail.com>
2023-02-08 23:41:24 +0700

    [Rewrite Backend] - Add endpoints for login, logout

--- backend/src/services/t_auth/index.ts
+++ backend/src/services/t_auth/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import handleLogin from "./controllers/handleLogin.js";
+import handleLogout from "./controllers/handleLogout.js";
 
 const router = Router();
+
+router.post("/login", handleLogin);
+router.put("/logout", handleLogout);
 
 export default router;
