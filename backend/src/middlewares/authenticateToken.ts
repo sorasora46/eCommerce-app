1363260fbd1577a3dc643e08e1fc0074019a3586
sora-k46 <sorrawit456789@gmail.com>
2023-02-10 23:11:08 +0700

    Remove checking refresh_token

--- backend/src/middlewares/authenticateToken.ts
+++ backend/src/middlewares/authenticateToken.ts
@@ -1,40 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 import jsonwebtoken from "jsonwebtoken";
 
-export const authenticateToken = (
+export default function authenticateToken(
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+) {
   try {
     const access_token = req.signedCookies.access_token;
-    const refresh_token = req.signedCookies.refresh_token;
 
-    // Redirect to login page when both tokens not found
-    if (!access_token && !refresh_token) {
-      return res.json({
-        redirectUrl: "http://127.0.0.1:5173/login",
-        unathorized: true,
-      });
-    }
-
-    // Verify the token when access token is not expired
     if (access_token) {
       const token = access_token.split(" ")[1];
       jsonwebtoken.verify(
         token,
-        "very secret access_token_secret string",
-        (err: any, user: any) => {
+        "replace me with secret from dotenv",
+        (err: any, _: any) => {
           if (err) throw err;
           next();
         }
       );
     }
 
-    // Proceed further if above conditions don't satisfied
-    next();
-  } catch (error: any) {
-    console.log(error);
-    res.json({ message: error.message });
+    res.status(401).send("not authenticated");
+  } catch (err: any) {
+    console.log(err.message);
+    res.send(err.message);
   }
-};
+}
