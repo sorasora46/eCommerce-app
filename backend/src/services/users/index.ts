183525bf862e46837ed07e0e880dbe642bfa81d6
sora-k46 <sorrawit456789@gmail.com>
2023-02-11 23:54:48 +0700

    Add missing import

--- backend/src/services/users/index.ts
+++ backend/src/services/users/index.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import blockUserById from "./controllers/blockUserById.js";
 import getUserById from "./controllers/getUserById.js";
+import reportUserById from "./controllers/reportUserById.js";
+import unBlockUserById from "./controllers/unBlockUserById.js";
 import _customerRoute from "./customer/index.js";
 import _shopRoute from "./shop/index.js";
 
