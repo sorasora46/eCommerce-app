804a8348fe454b314689e5b5d5c26417bc78c476
sora-k46 <sorrawit456789@gmail.com>
2023-02-11 23:59:08 +0700

    Add block list in user model

--- backend/src/services/users/models/user.model.ts
+++ backend/src/services/users/models/user.model.ts
@@ -9,12 +9,14 @@ export interface IUser {
   userId: string;
   email: string;
   role: Role;
+  blockList?: string[]; // list of userId
 }
 
 const userSchema = new Schema<IUser>({
   userId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   role: { type: String, required: true },
+  blockList: { type: [String] },
 });
 
 export const User = model("User", userSchema);
