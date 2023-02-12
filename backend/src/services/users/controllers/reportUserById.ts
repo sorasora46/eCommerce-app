e1e9a41103069fdf0167f2f7675729be4f402e86
sora-k46 <sorrawit456789@gmail.com>
2023-02-12 22:38:14 +0700

    Update reportUserById.ts

--- backend/src/services/users/controllers/reportUserById.ts
+++ backend/src/services/users/controllers/reportUserById.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from "express";
+import getUserInfoFromToken from "../../../helpers/getUserInfoFromToken.js";
+import { Report } from "../models/report.model.js";
 
 export default function reportUserById(req: Request, res: Response) {
   try {
+    const access_token = req.signedCookies.access_token;
+    const reportedUser = req.params.userId;
+    const { reason } = req.body;
+    getUserInfoFromToken(access_token, (err: any, user: any) => {
+      if (err) throw err;
+      Report.create(
+        {
+          reportBy: user.userId,
+          reportedUser: reportedUser,
+          reason: reason,
+        },
+        (err: any, result: any) => {
+          if (err) throw err;
+          return res.send(result);
+        }
+      );
+    });
   } catch (err: any) {
     console.log(err.message);
     res.send(err.message);
