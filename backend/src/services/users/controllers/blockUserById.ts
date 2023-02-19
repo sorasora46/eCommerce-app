d5dbd8e5e43023941e2449c0fbcdd8895f142c05
sora-k46 <sorrawit456789@gmail.com>
2023-02-19 21:38:47 +0700

    Verify userId without block logic

--- backend/src/services/users/controllers/blockUserById.ts
+++ backend/src/services/users/controllers/blockUserById.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from "express";
+import getUserInfoFromToken from "../../../helpers/getUserInfoFromToken.js";
+import { User } from "../models/user.model.js";
 
 export default function blockUserById(req: Request, res: Response) {
   try {
-    
+    const access_token = req.signedCookies.access_token;
+    const blockedUser = req.params.userId;
+    getUserInfoFromToken(access_token, async (err: any, user: any) => {
+      if (err) throw err;
+
+      const isBlockedUserExist = await User.findOne({ userId: blockedUser });
+      if (!isBlockedUserExist) return res.status(400).send("user not found");
+
+      if (blockedUser === user.userId)
+        return res.status(400).send("cannot block yourself");
+    // need block logic
+    });
   } catch (err: any) {
     console.log(err.message);
     res.send(err.message);
